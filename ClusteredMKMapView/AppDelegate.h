ba76a9795657fcf233b990db9141d14ab8761248
Swechha Prakash <swechhaprakash@gmail.com>
2015-07-09 13:11:10 +0530

    Initial Commit

---
+++ ClusteredMKMapView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClusteredMKMapView
+//
+//  Created by Swechha Prakash on 09/07/15.
+//  Copyright (c) 2015 Swechha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClusteredMKMapView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClusteredMKMapView
+//
+//  Created by Swechha Prakash on 09/07/15.
+//  Copyright (c) 2015 Swechha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
